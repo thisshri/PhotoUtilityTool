9ad641c1e20c1566c07beb8b872198c693a151dd
shri <thisshri@gmail.com>
2019-09-30 00:33:05 +0530

    WIP Add make photo box logic

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import sys
+from PIL import Image, ImageOps, ImageDraw
+
+
+def main(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+    # if file is passed,
+
+
+
+    # if folder is passed
+
+    ### using ops add border of pixels needed to padd
+    ### then crop the picture to make it a box ie 1:1
+    
+    ### thats all
+    import pdb; pdb.set_trace()
+
+if __name__ == "__main__":
+    sys.argv
+
+    # 
+    kwargs = sys.argv[2:]
+    iter_kwargs = iter(kwargs)
+    #
+    #
+    # way to specify wheather a file or folder is passed, FLAG
+    
+    main(sys.argv, kwargs)
