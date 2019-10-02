f44a203f5cdffde5916a40bbc560d6f47dab6968
shri <thisshri@gmail.com>
2019-10-02 17:38:21 +0530

    Fix lint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
-from PIL import Image, ImageOps, ImageDraw
+from PIL import Image
+
 
 def create_square_image(image):
     padding = round(
@@ -9,13 +10,18 @@ def create_square_image(image):
     top = right = bottom = left = 0
     if image.height > image.width:
         left = right = padding
-    else :
+    else:
         top = bottom = padding
 
     width = left + image.size[0] + right
     height = top + image.size[1] + bottom
-    square_image = Image.new(image.mode, (width, height), color=(255, 255, 255))
+    square_image = Image.new(
+        image.mode,
+        (width, height),
+        color=(255, 255, 255)
+        )
     square_image.paste(image, (left, top))
+
 
 def main(*args, **kwargs):
     print(args)
@@ -24,6 +30,7 @@ def main(*args, **kwargs):
     image = Image.open('foo.jpg')
     image = create_square_image(image)
     import pdb; pdb.set_trace()
+
 
 if __name__ == "__main__":
     sys.argv
