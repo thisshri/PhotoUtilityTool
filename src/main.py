a671b50529fe5bc3b5da2f8c3a3531a049c1793e
shri <thisshri@gmail.com>
2019-10-02 17:34:19 +0530

    Add create_square_image method

--- src/main.py
+++ src/main.py
@@ -1,32 +1,34 @@
 import sys
 from PIL import Image, ImageOps, ImageDraw
 
+def create_square_image(image):
+    padding = round(
+        abs(image.width - image.height) / 2
+    )
+
+    top = right = bottom = left = 0
+    if image.height > image.width:
+        left = right = padding
+    else :
+        top = bottom = padding
+
+    width = left + image.size[0] + right
+    height = top + image.size[1] + bottom
+    square_image = Image.new(image.mode, (width, height), color=(255, 255, 255))
+    square_image.paste(image, (left, top))
 
 def main(*args, **kwargs):
     print(args)
     print(kwargs)
 
-
-    # if file is passed,
-
-
-
-    # if folder is passed
-
-    ### using ops add border of pixels needed to padd
-    ### then crop the picture to make it a box ie 1:1
-    
-    ### thats all
+    image = Image.open('foo.jpg')
+    image = create_square_image(image)
     import pdb; pdb.set_trace()
 
 if __name__ == "__main__":
     sys.argv
 
-    # 
     kwargs = sys.argv[2:]
     iter_kwargs = iter(kwargs)
-    #
-    #
     # way to specify wheather a file or folder is passed, FLAG
-    
     main(sys.argv, kwargs)
